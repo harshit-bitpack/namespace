282e59ef2665b6afe71b7953c4c952a996c2976c
sanjit <132991552+thebitparticle@users.noreply.github.com>
2023-05-10 22:11:08 +0530

    made the landing page rewrite to webflow page (#27)

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,4 +5,12 @@ module.exports = {
   images: {
     domains: ["picsum.photos", "ipfs.thirdwebcdn.com"],
   },
+  async rewrites() {
+		return [
+			{
+				source: "/",
+				destination: "https://meroku-homepage.webflow.io/",
+			},
+		];
+	},
 };
