9f6babffea1f7603b792efa1d5b48aeb96ee2daa
harshit-bitpack <harshit.sharma@bitpack.me>
2023-05-16 21:52:21 +0530

    reserved App names list update

--- packages/contract/hardhat.config.js
+++ packages/contract/hardhat.config.js
@@ -79,7 +79,7 @@ module.exports = {
       live: true,
       saveDeployments: true,
       tags: ["staging"],
-      gasPrice: 1000000000,
+      gasPrice: 30000000000,
       gasMultiplier: 2,
     },
     polygon: {
